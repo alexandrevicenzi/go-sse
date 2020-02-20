32084b2c4b94e95fc53f16687d116f586092c72a
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2020-02-20 21:34:44 +0100

    Fix race condition

--- sse_test.go
+++ sse_test.go
@@ -2,6 +2,8 @@ package sse
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"sync"
 	"testing"
 )
@@ -28,53 +30,60 @@ func TestNewServerNilLogger(t *testing.T) {
 }
 
 func TestServer(t *testing.T) {
+	channelCount := 2
+	clientCount := 5
+	messageCount := 0
+
 	srv := NewServer(&Options{
-		Logger: nil,
+		Logger: log.New(os.Stdout, "go-sse: ", log.Ldate|log.Ltime|log.Lshortfile),
 	})
 
 	defer srv.Shutdown()
 
-	for n := 0; n < 2; n++ {
+	// Create N channes
+	for n := 0; n < channelCount; n++ {
 		name := fmt.Sprintf("CH-%d", n+1)
 		srv.addChannel(name)
-		// fmt.Printf("Channel %s registed\n", name)
+		fmt.Printf("Channel %s registed\n", name)
 	}
 
-	messages := make([]*Message, 0)
-	wgReg := sync.WaitGroup{}
-	wgCh := sync.WaitGroup{}
+	wg := sync.WaitGroup{}
+	m := sync.Mutex{}
 
-	for n := 0; n < 5; n++ {
+	// Create N clients in all channes
+	for n := 0; n < clientCount; n++ {
 		for name, ch := range srv.channels {
-			wgReg.Add(1)
-			wgCh.Add(1)
+			wg.Add(1)
+
+			// Create new client
+			c := newClient("", name)
+			// Add client to current channel
+			ch.addClient(c)
+
+			id := fmt.Sprintf("C-%d", n+1)
+			fmt.Printf("Client %s registed to channel %s\n", id, name)
 
 			go func(id string) {
-				c := newClient("", name)
-				ch.addClient(c)
-
-				// fmt.Printf("Client %s registed to channel %s\n", id, name)
-				wgReg.Done()
-
+				// Wait for messages in the channel
 				for msg := range c.send {
-					// fmt.Printf("Channel: %s - Client: %s - Message: %s\n", name, id, msg.data)
-					messages = append(messages, msg)
+					m.Lock()
+					messageCount += 1
+					m.Unlock()
+					fmt.Printf("Channel: %s - Client: %s - Message: %s\n", name, id, msg.data)
+					wg.Done()
 				}
-
-				wgCh.Done()
-			}(fmt.Sprintf("C-%d", n+1))
+			}(id)
 		}
 	}
 
-	wgReg.Wait()
-
+	// Send hello message to all channels and all clients in it
 	srv.SendMessage("", SimpleMessage("hello"))
 
 	srv.close()
 
-	wgCh.Wait()
+	wg.Wait()
 
-	if len(messages) != 10 {
-		t.Fail()
+	if messageCount != channelCount*clientCount {
+		t.Errorf("Expected %d messages but got %d", channelCount*clientCount, messageCount)
 	}
 }
